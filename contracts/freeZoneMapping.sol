e608be9a42a3d141b5201f739c2616bceba96ae3
Kali-Design <hm_leo@hotmail.com>
2022-01-15 16:50:55 +0100

    working progress

--- contracts/freeZoneMapping.sol
+++ contracts/freeZoneMapping.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-//import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract freeZone is ERC721 {
 
